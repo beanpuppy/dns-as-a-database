992f0f639ec2b1c503ecc7e1c4995cc360fe4a97
Justin Duch <justin@justinduch.com>
2020-02-24 22:01:58 +1100

    bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with io.open("README.md", "rt", encoding="utf8") as f:
 
 setup(
     name='daab',
-    version='0.1.0',
+    version='0.1.1',
     description='DNS as a database.',
     long_description=readme,
     long_description_content_type="text/markdown",
